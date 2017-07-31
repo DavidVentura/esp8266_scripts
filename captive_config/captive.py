d6144af4b215a4d2d085177c56155b4ad88702a9
David <davidventura27@gmail.com>
2017-07-30 21:43:10 -0300

    initial working captive config

--- captive_config/captive.py
+++ captive_config/captive.py
@@ -3,22 +3,32 @@ import network
 import machine
 
 
+def wait_until_connected(wlan):
+    count = 0
+    while not wlan.isconnected() and count < 10:
+        print("Waiting...")
+        time.sleep(1)
+        count += 1
+
+    return wlan.isconnected()
+
+
 def do_connect():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        count = 0
-        while not wlan.isconnected() and count < 10:
-            print("Waiting...")
-            time.sleep(1)
-            count += 1
+    print('connecting to network...')
+    conn = wait_until_connected(wlan)
 
-    if not wlan.isconnected():
+    if not conn:
         import http
         ssid, password = http.serve('hey pls config me')
+        print("Data provided:", ssid, password)
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
         wlan.connect(ssid, password)
+
         print("Resetting!")
+        wait_until_connected(wlan)
         time.sleep(2)
         print("Resetting!")
         machine.reset()
--- captive_config/http.py
+++ captive_config/http.py
@@ -4,8 +4,33 @@ import time
 s = None
 
 
+def parse_req(req):
+    # req = [ "GET /toggle HTTP/1.1", ....]
+    req = req[0]
+    if " " not in req:
+        return None, None
+    # r = "GET /toggle?asd=3&bbb=4 HTTP/1.1"
+    r = req.split(" ")[1]
+    # r = "/toggle?asd=3&bbb=4"
+    path = r[1:]
+    # path = "toggle?asd=3&bbb=4"
+    tmp = path.split("?")
+    endpoint = tmp[0]
+    params = {}
+    if len(tmp) > 1:
+        s_params = tmp[1]
+        # s_params = asd=3&bbb=4
+        params = {}
+        for param in s_params.split("&"):
+            # param = 'asd=3' .. 'bbb=4'
+            key, value = param.split('=')
+            params[key] = value
+
+    return endpoint, params
+
+
 def debug(*msg):
-    if False:
+    if True:
         print(*msg)
 
 
@@ -33,9 +58,15 @@ def serve(data):
             debug(r)
             debug("*" * 20, "Request", "*" * 20)
 
+            path, params = parse_req(r)
+            debug("PATH: ", path)
+            debug("PARAMS: ", params)
+
             cl.send("HTTP/1.1 200 OK\r\n\r\n")
             cl.write(data)
             cl.close()
+            if path == 'config':
+                return 'right in the kokoro', 'tacotaco'
         except OSError:
             # timeouts
             pass
