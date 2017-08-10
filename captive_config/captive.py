b328189f6bfd19fa50c0d5856b712f65128c65a9
David <davidventura27@gmail.com>
2017-08-10 19:00:14 -0300

    serve ssid list

--- captive_config/captive.py
+++ captive_config/captive.py
@@ -13,27 +13,49 @@ def wait_until_connected(wlan):
     return wlan.isconnected()
 
 
+def config(ssid, password):
+    ssid = ssid.replace('+', ' ')
+    print("Data provided:", ssid, password)
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    print("Resetting!")
+    wait_until_connected(wlan)
+    time.sleep(2)
+    print("Resetting!")
+    machine.reset()
+
+
+def list_ssids():
+    ret = []
+    for item in get_ssids_and_qual():
+        ret.append('''<p>
+                        <label>
+                            <input type="radio" name="ssid" value="%s">
+                            <b>%s</b> <small>%d%%</small>
+                        </label>
+                      </p>\n''' % (item[0], item[0], item[1]))
+    data = ''.join(ret)
+    s = open('ssidlist.html').read().replace('{{list}}', data)
+    return s
+
+
 def do_connect():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     print('connecting to network...')
     conn = wait_until_connected(wlan)
 
-    if not conn:
+    TESTING = True
+    if not conn or TESTING:
+        setup_ap()
         import http
-        ssid, password = http.serve('hey pls config me')
-        print("Data provided:", ssid, password)
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-        wlan.connect(ssid, password)
-
-        print("Resetting!")
-        wait_until_connected(wlan)
-        time.sleep(2)
-        print("Resetting!")
-        machine.reset()
+        http.serve({'': list_ssids,
+                    'config': config})
 
     else:
+        disable_ap()
         print('network config:', wlan.ifconfig())
 
 
@@ -49,3 +71,15 @@ def get_ssids_and_qual():
 
 def rssi_to_perc(rssi):
     return min(max(2 * (rssi + 100), 0), 100)
+
+
+def setup_ap():
+    ap = network.WLAN(network.AP_IF)
+    ap.active(True)
+    ap.config(essid="Test_SSID_Please_config", authmode=1)
+    print("AP set up")
+
+
+def disable_ap():
+    ap = network.WLAN(network.AP_IF)
+    ap.active(True)
